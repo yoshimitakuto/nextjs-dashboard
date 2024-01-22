f69cf7387ffec3ecc2f8fc64a5fecbb18774a097
YoshimiTakuto <takuto.yoshimi.ncdc.co.jp>
2024-01-22 18:19:37 +0900

    check: deploy

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -9,4 +9,6 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
     </div>
   )
-}
+};
+
+// check deployment
